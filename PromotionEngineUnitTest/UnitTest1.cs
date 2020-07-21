8a54b592d0e5052bcaff319ea930f32e4647b5c2
SuthagarThambi <66585356+SuthagarThambi@users.noreply.github.com>
2020-07-21 12:28:12 +0530

    UnitTestAdded

---
+++ PromotionEngineUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PromotionEngineUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
