d27305d1e3ae0a7fd237743aec5cef4088f63282
SuthagarThambi <66585356+SuthagarThambi@users.noreply.github.com>
2020-07-21 13:01:06 +0530

    Mock

---
+++ PromotionEngineUnitTest/MockData.cs
@@ -0,0 +1,43 @@
+﻿using PromotionEngine.Business.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace PromotionEngineUnitTest
+{
+    public static class MockData
+    {
+        public static List<PurchaseDetails> GetScenario1()
+        {
+            List<PurchaseDetails> purchaseDetails = new List<PurchaseDetails>()
+            {
+                new PurchaseDetails(){SKU_Name="A",SKU_Quantity=3},
+                new PurchaseDetails(){SKU_Name="B",SKU_Quantity=5},
+                new PurchaseDetails(){SKU_Name="C",SKU_Quantity=1},
+                new PurchaseDetails(){SKU_Name="D",SKU_Quantity=1},
+            };
+            return purchaseDetails;
+        }
+        public static List<PurchaseDetails> GetScenario2()
+        {
+            List<PurchaseDetails> purchaseDetails = new List<PurchaseDetails>()
+            {
+                new PurchaseDetails(){SKU_Name="A",SKU_Quantity=5},
+                new PurchaseDetails(){SKU_Name="B",SKU_Quantity=5},
+                new PurchaseDetails(){SKU_Name="C",SKU_Quantity=1},
+            };
+            return purchaseDetails;
+        }
+        public static List<PurchaseDetails> GetScenario3()
+        {
+            List<PurchaseDetails> purchaseDetails = new List<PurchaseDetails>()
+            {
+                new PurchaseDetails(){SKU_Name="A",SKU_Quantity=1},
+                new PurchaseDetails(){SKU_Name="B",SKU_Quantity=1},
+                new PurchaseDetails(){SKU_Name="C",SKU_Quantity=1},
+            };
+            return purchaseDetails;
+        }
+    }
+}
--- PromotionEngineUnitTest/UnitTest1.cs
+++ PromotionEngineUnitTest/UnitTest1.cs
@@ -1,14 +1,59 @@
+using PromotionEngine.Business.Contracts;
+using PromotionEngine.Business.Contracts.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace PromotionEngineUnitTest
 {
+   
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        private IBusinessManager _businessManager;
+        public UnitTest1()
         {
-
+            _businessManager = new BusinessManager();
+        }
+        [Fact]
+        public void TestScenario1()
+        {
+            List<PurchaseDetails> purchaseDetails = MockData.GetScenario1();
+            var response=  _businessManager.CheckOut(purchaseDetails);
+            int Aval = response.Where(i => i.SKU_Name == "A").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Bval = response.Where(i => i.SKU_Name == "B").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Cval = response.Where(i => i.SKU_Name == "C").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Dval = response.Where(i => i.SKU_Name == "D").Select(i => i.SKU_PRICE).FirstOrDefault();
+            Assert.Equal(130, Aval);
+            Assert.Equal(120, Bval);
+            Assert.Equal(0, Cval);
+            Assert.Equal(30, Dval);
+        }
+        [Fact]
+        public void TestScenario2()
+        {
+            List<PurchaseDetails> purchaseDetails = MockData.GetScenario2();
+            var response = _businessManager.CheckOut(purchaseDetails);
+            int Aval = response.Where(i => i.SKU_Name == "A").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Bval = response.Where(i => i.SKU_Name == "B").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Cval = response.Where(i => i.SKU_Name == "C").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Dval = response.Where(i => i.SKU_Name == "D").Select(i => i.SKU_PRICE).FirstOrDefault();
+            Assert.Equal(230, Aval);
+            Assert.Equal(120, Bval);
+            Assert.Equal(20, Cval);
+        }
+        [Fact]
+        public void TestScenario3()
+        {
+            List<PurchaseDetails> purchaseDetails = MockData.GetScenario3();
+            var response = _businessManager.CheckOut(purchaseDetails);
+            int Aval = response.Where(i => i.SKU_Name == "A").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Bval = response.Where(i => i.SKU_Name == "B").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Cval = response.Where(i => i.SKU_Name == "C").Select(i => i.SKU_PRICE).FirstOrDefault();
+            var Dval = response.Where(i => i.SKU_Name == "D").Select(i => i.SKU_PRICE).FirstOrDefault();
+            Assert.Equal(50, Aval);
+            Assert.Equal(30, Bval);
+            Assert.Equal(20, Cval);
         }
     }
 }
